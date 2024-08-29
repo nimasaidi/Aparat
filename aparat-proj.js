772532171f97c6b6678a96044d38ab42a1bec737
nimasaidi <nimasaidi1385@gmail.com>
2024-08-29 20:25:53 +0330

    Add files via upload

--- aparat-proj.js
+++ aparat-proj.js
@@ -1,4 +1,17 @@
-video=getElementsByTagName('video');
-{
-    video.removeAttribute('controls');
-}
+var video = document.getElementsByTagName('video')[0];
+console.log(video)
+video.removeAttribute('controls');
+
+
+//
+
+
+var play = document.getElementById("playvideo");
+play.addEventListener("click", function () {
+    video.play()
+    video.requestFullscreen()
+})
+
+// array in js
+// DOM - variables - if satements ( if , inline if  , switch) - loop( map , foreach ,for)
+//js selectors -- event listeners
